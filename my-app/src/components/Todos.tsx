00cf317c8fa8a7a89192d96d07988141cefe11b2
minji.jo <mwinterj@daum.net>
2023-03-14 13:54:18 +0900

    Feat: Todos 컴포넌트에 props 타입 지정하기

--- my-app/src/components/Todos.tsx
+++ my-app/src/components/Todos.tsx
@@ -1,10 +1,18 @@
-function Todos() {
+import React from "react";
+
+/**
+ * 리액트의 컴포넌트를 제네릭 타입의 함수로 지정하여 사용
+ * FC : Functional Component
+**/
+
+const Todos: React.FC<{ items: string[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
