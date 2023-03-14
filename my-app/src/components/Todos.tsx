d40cc0a0560e138ce390f3ff0d81bc08957acc8d
minji.jo <mwinterj@daum.net>
2023-03-14 15:45:46 +0900

    Feat: Todo class로 만든 array를 타입으로 지정

--- my-app/src/components/Todos.tsx
+++ my-app/src/components/Todos.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import Todo from "../models/todo";
 
 /**
  * 리액트의 컴포넌트를 제네릭 타입의 함수로 지정하여 사용
  * FC : Functional Component
-**/
+ **/
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
+// Todo class로 만든 배열을 props로 받는다.
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
