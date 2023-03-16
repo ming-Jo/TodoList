f02af1ed5db27542d130e3bc373ca90e6f242493
minji.jo <mwinterj@daum.net>
2023-03-17 01:08:53 +0900

    Feat: 추가된 Todo 항목 클릭 시 삭제되도록 기능 추가 및 타입 지정

--- my-app/src/components/TodoListItem.tsx
+++ my-app/src/components/TodoListItem.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import classes from "./TodoListItem.module.css";
 
-const TodoListItem: React.FC<{ text: string }> = (props) => {
-  return <li>{props.text}</li>;
+// 타입에서 onRemoveTodo: (event: React.MouseEvent) => void; 매개변수의 값을 마우스이벤트로 지정할 수도 있지만 생략해도 됨
+const TodoListItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (
+  props
+) => {
+  return (
+    <li onClick={props.onRemoveTodo} className={classes.item}>
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoListItem;
--- my-app/src/components/Todos.tsx
+++ my-app/src/components/Todos.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Todo from "../models/todo";
 import TodoListItem from "./TodoListItem";
+import classes from "./Todos.module.css";
 
 /**
  * 리액트의 컴포넌트를 제네릭 타입의 함수로 지정하여 사용
@@ -9,11 +10,20 @@ import TodoListItem from "./TodoListItem";
 
 // Todo class로 만든 배열을 props로 받는다.
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+// TodoListItem의 onRemoveTodo에 들어갈 함수를 App.tsx에서 정의하기 위해 다시 props로 전달한다.
+
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoListItem key={item.id} text={item.text} />
+        <TodoListItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+          // bind() : 첫번째 인자=호출될 함수 내에서 무엇을 가리키는지, 두번째 인자=onRemoveTodo()가 매개변수로 받을 것
+        />
       ))}
     </ul>
   );
