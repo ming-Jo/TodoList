8438427401c1b04fb1c0c9f120d1cce7666b0ad2
minji.jo <mwinterj@daum.net>
2023-03-18 21:17:31 +0900

    Feat: 컴포넌트에 context API 적용

--- my-app/src/components/Todos.tsx
+++ my-app/src/components/Todos.tsx
@@ -1,27 +1,18 @@
-import React from "react";
-import Todo from "../models/todo";
+import React, { useContext } from "react";
 import TodoListItem from "./TodoListItem";
+import { TodosContext } from "../store/todos-context";
 import classes from "./Todos.module.css";
 
-/**
- * 리액트의 컴포넌트를 제네릭 타입의 함수로 지정하여 사용
- * FC : Functional Component
- **/
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
 
-// Todo class로 만든 배열을 props로 받는다.
-
-// TodoListItem의 onRemoveTodo에 들어갈 함수를 App.tsx에서 정의하기 위해 다시 props로 전달한다.
-
-const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
-  props
-) => {
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
+      {todosCtx.items.map((item) => (
         <TodoListItem
           key={item.id}
           text={item.text}
-          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
           // bind() : 첫번째 인자=호출될 함수 내에서 무엇을 가리키는지, 두번째 인자=onRemoveTodo()가 매개변수로 받을 것
         />
       ))}
